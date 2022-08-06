0fd80ffada47a6ebd818da7e68c51ce93d42e29b
innotechBymoen <alexb@innotechcollege.com>
2022-08-06 15:23:53 -0600

    Added my header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h3>LOGO HERE</h3>
+        <nav>
+            <a href="">LINK ONE</a>
+            <a href="">LINK TWO</a>
+            <a href="">LINK THREE</a>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    header {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        place-items: center;
+    }
+
+    nav {
+        justify-self: end;
+        display: grid;
+        place-items: center;
+        grid-auto-flow: column;
+        gap: 10px;
+    }
+</style>
